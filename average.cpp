5eea609a8c045c80b9701d3e89e73aa374248137
Your Name <you@example.com>
2023-03-03 14:59:01 +0700

    finish lab23.1

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+    if(argc==1) {
+        cout<<"Please input numbers to find average.";
+        return 0;
+    }
+    double x;
+    for(int i=0;i<argc;i++){
+        x+=atoi(argv[i]);
+    }
+    cout<<"---------------------------------\nAverage of "<<argc-1<<" numbers = "<<x/(argc-1)<<"\n---------------------------------";
+    return 0;
+}
